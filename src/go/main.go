1b24dfdb73ab2d0c88777a8b052d73854daf6c8c
Pete de Zwart <dezwart@gmail.com>
2011-11-25 00:24:58 +1100

    Easiest sieve of eratosthenes I've done, sweet. Go go\!

--- src/go/main.go
+++ src/go/main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+func printPrime(prime int) {
+    fmt.Println(prime, "is prime")
+}
+
 func main() {
-    fmt.Printf("hello, world\n")
+    max_num := 1073741824
+    numbers := make([]bool, max_num)
+
+    printPrime(1)
+    for i := 2 ; i < max_num; i++ {
+        if numbers[i] == false {
+            printPrime(i)
+        }
+
+        for j := i; j < max_num; j += i {
+            numbers[j] = true
+        }
+    }
 }
