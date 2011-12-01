caf08df8c9cf4a5da532c60ef5cca1f41a06c265
Pete de Zwart <dezwart@gmail.com>
2011-12-01 20:36:08 +1100

    1 is not a prime.

--- src/prime/go/sieve.go
+++ src/prime/go/sieve.go
@@ -9,15 +9,17 @@ func printPrime(prime int) {
 func main() {
     max_num := 1073741824
     numbers := make([]bool, max_num)
+    num_primes := 0
 
-    printPrime(1)
     for i := 2; i < max_num; i++ {
         if numbers[i] == false {
-            printPrime(i)
+            num_primes++
         }
 
         for j := i; j < max_num; j += i {
             numbers[j] = true
         }
     }
+
+    fmt.Println(num_primes, "out of", max_num, "are primes.")
 }
