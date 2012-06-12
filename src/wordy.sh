55edf298c253bd8bfc87490f527ff2ea8aa0133b
Pete de Zwart <dezwart@gmail.com>
2012-06-13 00:50:10 +1000

    What did you say?

---
+++ src/wordy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Amuse/annoy with random words at random intervals.
+# Great for breaking the ice at parties.
+# Needs `say`, try on a Mac.
+
+declare -a words
+
+i=0
+
+for word in `cat /usr/share/dict/words`
+do
+    words[$i]="$word"
+    let i+=1
+done
+
+while true
+do
+    index=$(($RANDOM*$RANDOM%$i))
+    word="${words[$index]}"
+    sleepy=$RANDOM
+    echo "Sleeping for $sleepy to say $word"
+    sleep $RANDOM
+    say "$word"
+done
